1fa007224b350078119185fd290298230a3159b1
vasilis <vasilis12.manetas@gmail.com>
2022-07-31 14:38:00 +0200

    improve code

--- passwortgenerator.py
+++ passwortgenerator.py
@@ -11,48 +11,17 @@ def silbeGenerieren() :
     return silbe
 
 case = rnd.randint(0,2)
+Zahl = str(rnd.randint(0,9))
+
 if case == 0:
-    Zahl = str(rnd.randint(0,9))
-    zahlPos = rnd.randint(0,3)
-    if zahlPos == 0:
-        Passwort = Zahl + silbeGenerieren() + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 1:
-        Passwort =  silbeGenerieren() + Zahl + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 2:
-        Passwort =  silbeGenerieren() + silbeGenerieren()+ Zahl + silbeGenerieren()
-    elif zahlPos == 3:
-        Passwort =  silbeGenerieren() + silbeGenerieren()+ silbeGenerieren() + Zahl
+    Passwort = [silbeGenerieren(), silbeGenerieren(), silbeGenerieren()]
+    Passwort.insert(rnd.randint(0,3), Zahl)
 elif case == 1:
-    Zahl = str(rnd.randint(0,9))
-    zahlPos = rnd.randint(0,5)
-    if zahlPos == 0:
-        Passwort = Zahl + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 1:
-        Passwort = silbeGenerieren() + Zahl + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 2:
-        Passwort = silbeGenerieren() + silbeGenerieren() + Zahl + silbeGenerieren() + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 3:
-        Passwort = silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + Zahl + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 4:
-        Passwort = silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + Zahl + silbeGenerieren()
-    elif zahlPos == 5:
-        Passwort = silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + Zahl
+    Passwort = [silbeGenerieren(), silbeGenerieren(), silbeGenerieren(), silbeGenerieren(), silbeGenerieren()]
+    Passwort.insert(rnd.randint(0,5), Zahl)
 elif case == 2:
-    zahlPos = rnd.randint(0,6)
-    Zahl = str(rnd.randint(0,9)) + str(rnd.randint(0,9))
-    if zahlPos == 0:
-        Passwort = Zahl + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 1:
-        Passwort = silbeGenerieren() + Zahl + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() 
-    elif zahlPos == 2:
-        Passwort = silbeGenerieren() + silbeGenerieren() + Zahl + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 3:
-        Passwort = silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + Zahl + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() 
-    elif zahlPos == 4:
-        Passwort = silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + Zahl + silbeGenerieren() + silbeGenerieren()
-    elif zahlPos == 5:
-        Passwort = silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + Zahl + silbeGenerieren()
-    elif zahlPos == 6:
-        Passwort = silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + silbeGenerieren() + Zahl
-    
-print(Passwort)
+    Zahl = Zahl + str(rnd.randint(0,9))
+    Passwort = [silbeGenerieren(), silbeGenerieren(), silbeGenerieren(), silbeGenerieren(),silbeGenerieren(), silbeGenerieren()]
+    Passwort.insert(rnd.randint(0,6), Zahl)
+
+print(''.join(str(x) for x in Passwort))
